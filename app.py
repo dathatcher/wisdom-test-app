8da44bd3807e3dce73469adf5355f4929193adb9
dathatcher <david.austin.thatcher@gmail.com>
2025-06-21 07:48:05 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "👋 Hello, World! This is my Wisdom Layer test app(5)."
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
